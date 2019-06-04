0306312297520e12af2a1303940f4760e0026b30
Amit Tomar <theamittomar@gmail.com>
2019-06-04 12:27:53 +0530

    Add project files.

---
+++ LogOffSystem/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace LogOffSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogOff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLogOff
+            // 
+            this.btnLogOff.Location = new System.Drawing.Point(164, 72);
+            this.btnLogOff.Name = "btnLogOff";
+            this.btnLogOff.Size = new System.Drawing.Size(136, 29);
+            this.btnLogOff.TabIndex = 0;
+            this.btnLogOff.Text = "Forced LogOff";
+            this.btnLogOff.UseVisualStyleBackColor = true;
+            this.btnLogOff.Click += new System.EventHandler(this.btnLogOff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 315);
+            this.Controls.Add(this.btnLogOff);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogOff;
+    }
+}
+
---
+++ LogOffSystem/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace LogOffSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.dll")]
+        public static extern int ExitWindowsEx(int operationFlag, int operationReason);
+        private void btnLogOff_Click(object sender, EventArgs e)
+        {
+            ExitWindowsEx(4, 0);
+        }
+    }
+}
